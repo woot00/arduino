6f763d29b1d87ec9f3216a09c5931456d3eefa9b
woot00 <127732600+woot00@users.noreply.github.com>
2023-04-17 19:02:40 +0900

    Add files via upload

---
+++ cds_lcd/cds_lcd.ino
@@ -0,0 +1,19 @@
+int #include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+cds = A0; // Analog input pin that the potentiometer is attached to
+int cdsValue = 0; // value read from the pot
+void setup() {
+  lcd.init();                      // initialize the lcd 
+  lcd.init();
+  // Print a message to the LCD.
+  lcd.backlight();
+  
+}
+void loop() {
+cdsValue  = analogRead(cds); // read the analog in value:
+lcd.setCursor(3,0);
+lcd.print("cds:");
+lcd.print(cdsValue);
+}
