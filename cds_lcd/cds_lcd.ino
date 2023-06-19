f0432c16fcaf99465029c0c48dd553eed11f9696
woot00 <127732600+woot00@users.noreply.github.com>
2023-06-19 17:52:38 +0900

    cds_lcd.ino

--- cds_lcd/cds_lcd.ino
+++ cds_lcd/cds_lcd.ino
@@ -1,19 +1,23 @@
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+#include <Wire. h>
+#include <LiquidCrystal I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-int cds = A0; 
-int cdsValue = 0 ; // value read from the pot
+LiquidCrystal_I2C led(0x27,16,2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+int sensorPin= Ad; // 조도 센서 연결 핀 
+int sensorValue= 0; // 측정된 조도 값
+
 void setup() {
-  lcd.init();                      // initialize the lcd 
-  lcd.init();
-  // Print a message to the LCD.
-  lcd.backlight();
-  
+lcd.init();
+lcd.init();
+1cd.backlight();
 }
+
 void loop() {
-cdsValue  = analogRead(cds); // read the analog in value:
-lcd.setCursor(3,0);
-lcd.print("cds:");
-lcd.print(cdsValue);
+sensorValue = analogRead(sensorpin); // 조도 센서 값을 측정
+1cd. setCursor (6,0);
+1cd.setCursor (0,0) ;
+Icd.print("cds : ");
+lcd.print(sensorValue);
+delay (1000);
+lcd.clear();
 }
